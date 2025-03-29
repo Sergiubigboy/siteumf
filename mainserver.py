b329ac39e2155297c27606ddf777876ca7ae2b78
Sergiubigboy <sergiubiris09@gmail.com>
2025-03-29 14:19:38 +0200

    am adaugat multle nav etc

--- mainserver.py
+++ mainserver.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def mainpage():
-    return render_template('mainpage.html', title="Bine ați venit!")
+    return render_template('mainpage.html', subtitlu="Acasa", title="Bine ați venit!")
 
 @app.route('/despre')
 def despre():
-    return render_template('despre.html')
+    return render_template('despre.html', subtitlu="Despre", title="Despre Noi")
 
-app.run(debug=True)
+@app.route('/elevi')
+def elevi():
+    return render_template('elevi.html', subtitlu="Elevi", title="Elevi")
+
+@app.route('/profesori')
+def profesori():
+    return render_template('profesori.html', subtitlu="Profesori", title="Profesori")
+
+@app.route('/echohub')
+def echohub():
+    return render_template('echohub.html', subtitlu="Echohub", title="Echohub")
+
+@app.route('/premii')
+def premii():
+    return render_template('premii.html', subtitlu="Premii", title="Premii")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
