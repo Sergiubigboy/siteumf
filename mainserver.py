b58923e2214fd85b6ed3a851385259c968d1294c
Sergiubigboy <sergiubiris09@gmail.com>
2025-03-29 12:41:38 +0200

    ceva mici modificari

--- mainserver.py
+++ mainserver.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def mainpage():
-    return render_template('mainpage.html')
+    return render_template('mainpage.html', title="Bine ați venit!")
+
+@app.route('/despre')
+def despre():
+    return render_template('despre.html')
 
 app.run(debug=True)
