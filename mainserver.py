c0df2e751ee4a225177cc837c305e455b3858bd3
Sergiubigboy <sergiubiris09@gmail.com>
2025-03-31 15:42:45 +0300

    Renamed routes for 'despre', 'profesori', and 'premii' to 'revista', 'corp-profesoral', and 'podcast' respectively; added a new 'date.py' file with a professor catalog and search functionality; updated CSS styles in 'despre.css' and 'layout.html'.

--- mainserver.py
+++ mainserver.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
+from date import CatalogPersonal, profesori
 
+Profesori = CatalogPersonal(profesori)
 app = Flask(__name__)
 
 @app.route('/')
 def mainpage():
     return render_template('mainpage.html', subtitlu="Acasa", title="Bine ați venit!", background="bgmain.png")
 
-@app.route('/despre')
+@app.route('/revista')
 def despre():
     return render_template('despre.html', subtitlu="Despre", title="Despre Noi", background="liceu2.jpeg")
 
@@ -14,7 +16,7 @@ def despre():
 def elevi():
     return render_template('elevi.html', subtitlu="Elevi", title="Elevi", background="bgmain.png")
 
-@app.route('/profesori')
+@app.route('/corp-profesoral')
 def profesori():
     return render_template('profesori.html', subtitlu="Profesori", title="Corp Profesoral", background="bgmain.png")
 
@@ -22,9 +24,9 @@ def profesori():
 def echohub():
     return render_template('echohub.html', subtitlu="Echohub", title="Echohub", background="bgmain.png")
 
-@app.route('/premii')
+@app.route('/podcast')
 def premii():
-    return render_template('premii.html', subtitlu="Premii", title="Premii", background="bgmain.png")
+    return "SOON!!!"
 
 
 
